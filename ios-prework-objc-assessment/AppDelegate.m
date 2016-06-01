5423614a695eef2d8ff9bef48396f61d1dd78682
github username <github email address>
2016-05-31 22:17:39 -0400

    assessment completed

--- ios-prework-objc-assessment/AppDelegate.m
+++ ios-prework-objc-assessment/AppDelegate.m
@@ -16,7 +16,60 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    NSArray *numbersArray = @[];
+    NSArray *lettersArray = @[];
+    NSDictionary *alphabetDictionary = @{};
+    
+    numbersArray = [self generateArrayOfNumbers];
+    lettersArray = [self generateArrayOfLetters];
+    alphabetDictionary = [self generateNumbersAndLettersDictionaryWithNumbers:numbersArray letters:lettersArray];
+    
+    for (int i=0; i < [numbersArray count]; i++) {
+        NSLog(@"%@ : %@", numbersArray[i], alphabetDictionary[numbersArray[i]]);
+    }
+    
+    /*
+     Alternative logging with no guarantee of ordered dictionary log:
+     NSLog(@"%@",alphabetDictionary);
+    */
+    
+    numbersArray = [alphabetDictionary allKeys];
+    
+    NSMutableArray *mutableLettersArray = [lettersArray mutableCopy];
+    [mutableLettersArray removeAllObjects];
+    
+    for (NSNumber *key in alphabetDictionary) {
+        [mutableLettersArray addObject:alphabetDictionary[key]];
+    }
+    
+    NSLog(@"%@\n%@",numbersArray,mutableLettersArray);
+    
     return YES;
 }
+
+- (NSArray *)generateArrayOfNumbers {
+    NSMutableArray *mNumbersArray = [[NSMutableArray alloc] init];
+    for (int i=1; i <= 26; i++) {
+        [mNumbersArray addObject:@(i)];
+    }
+    return [mNumbersArray copy];
+}
+
+- (NSArray *)generateArrayOfLetters {
+    NSMutableArray *mLettersArray = [[NSMutableArray alloc] init];
+    for (char c = 'a'; c <= 'z'; c++) {
+        [mLettersArray addObject:[NSString stringWithFormat:@"%c",c]];
+    }
+    return [mLettersArray copy];
+}
+
+- (NSDictionary *)generateNumbersAndLettersDictionaryWithNumbers:(NSArray *)numbersArray letters:(NSArray *)lettersArray {
+    NSMutableDictionary *mAlphabetDictionary = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [numbersArray count]; i++) {
+        mAlphabetDictionary[numbersArray[i]] = lettersArray[i];
+    }
+    return [mAlphabetDictionary copy];
+}
+
 
 @end
